8ddf318fc12e46cffc4f3c41e3852f0647ac4132
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 21:03:50 +0300

    1. Sayfadaki görsel nesneler Main Activity de tanımlandı.

--- app/src/main/java/com/ogocer/widgets/MainActivity.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity.java
@@ -3,12 +3,30 @@ package com.ogocer.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etIsim;
+    private TextView tvIsimYaz,tvSonucYaz;
+    private Button btnIsimAl,btnIntent;
+    private ToggleButton toggleButton;
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etIsim = findViewById(R.id.etIsim);
+        tvIsimYaz = findViewById(R.id.textViewIsimYaz);
+        tvSonucYaz = findViewById(R.id.textViewToggleSwitch);
+        btnIsimAl = findViewById(R.id.buttonIsimAl);
+        btnIntent = findViewById(R.id.buttonIntent);
+        toggleButton = findViewById(R.id.toggleButton);
+        aSwitch = findViewById(R.id.switch1);
     }
 }
