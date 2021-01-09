bd865e2f8fd51b209031555c779df993e3f7f849
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 21:50:27 +0300

    Main Activity 3 oluşturuldu. 2. sayfadan 3. sayfaya geçiş intenti yapıldı.

--- app/src/main/java/com/ogocer/widgets/MainActivity2.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity2.java
@@ -2,7 +2,9 @@ package com.ogocer.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Matrix2f;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -64,5 +66,14 @@ public class MainActivity2 extends AppCompatActivity {
                 }
             }
         });
+
+        btnIntentTo3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this,MainActivity3.class));
+            }
+        });
+
+
     }
 }
