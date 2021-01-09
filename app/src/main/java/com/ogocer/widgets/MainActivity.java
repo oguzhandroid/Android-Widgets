a9959ac59c41f7c6170ea0c57ebd88afa588ca1f
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 21:22:14 +0300

    Yeni Activity oluşturuldu ve 1. Sayfadaki intent butonunda gerekli intent işlemi yapıldı

--- app/src/main/java/com/ogocer/widgets/MainActivity.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity.java
@@ -2,6 +2,7 @@ package com.ogocer.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -70,6 +71,13 @@ public class MainActivity extends AppCompatActivity {
                 }else if (!isChecked && !switchKontrol){
                     tvSonucYaz.setText("Switch : Pasif\nToggle : Pasif");
                 }
+            }
+        });
+
+        btnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
             }
         });
 
