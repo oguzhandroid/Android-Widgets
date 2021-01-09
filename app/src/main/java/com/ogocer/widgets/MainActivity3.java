ebbda73481eb6399a92ba2417453e7651fd35d9f
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 22:39:07 +0300

    Progress Bar çalıştırma ve durdurma butonları tamamlandı.

--- app/src/main/java/com/ogocer/widgets/MainActivity3.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity3.java
@@ -3,6 +3,7 @@ package com.ogocer.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
@@ -29,5 +30,18 @@ public class MainActivity3 extends AppCompatActivity {
         seekBar = findViewById(R.id.seekBar);
         tvPuan = findViewById(R.id.tvPuan);
         tvSeekbar = findViewById(R.id.tvSeekbar);
+
+        btnCalistir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+        btnDurdur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
