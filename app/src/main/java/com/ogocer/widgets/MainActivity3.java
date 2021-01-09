a473565b708255d20f36bb0e883534513fe1d39c
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 23:02:33 +0300

    unutulan işlem düzeltildi : Sayfa 3 ten sayfa 4 e geçme işlemi.

--- app/src/main/java/com/ogocer/widgets/MainActivity3.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity3.java
@@ -2,6 +2,7 @@ package com.ogocer.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -67,6 +68,13 @@ public class MainActivity3 extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        btnIntentTo4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity3.this,MainActivity4.class));
             }
         });
     }
