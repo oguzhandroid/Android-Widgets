f4a37f20ea9318c55deee6a88f31af3b5a8508aa
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 23:46:35 +0300

    5. sayfadaki görsel nesneler MainActivity5 te tanımlandı.

--- app/src/main/java/com/ogocer/widgets/MainActivity5.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity5.java
@@ -3,12 +3,21 @@ package com.ogocer.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity5 extends AppCompatActivity {
+    private TextView tvSaat,tvTarih;
+    private EditText etSaat,etTarih;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        tvSaat = findViewById(R.id.tvSaat);
+        tvTarih = findViewById(R.id.tvTarih);
+        etSaat = findViewById(R.id.etSaat);
+        etTarih = findViewById(R.id.etTarih);
     }
 }
