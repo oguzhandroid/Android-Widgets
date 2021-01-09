7b072b10a65730a87d4b0936f5a9445064cc66fa
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 22:59:05 +0300

    4. sayfa görsel nesneleri MainActivity4 de tanımlandı.

--- app/src/main/java/com/ogocer/widgets/MainActivity4.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity4.java
@@ -3,12 +3,28 @@ package com.ogocer.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity4 extends AppCompatActivity {
+    private ImageView imgView;
+    private VideoView videoView;
+    private Button btnResim1,btnResim2,btnIntentto5,btnVideoBaslat,btnVideoDurdur,btnVideoDevam,btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        imgView = findViewById(R.id.imageView);
+        videoView = findViewById(R.id.videoView);
+        btnIntentto5 = findViewById(R.id.btnIntentto5);
+        btnVideoBaslat = findViewById(R.id.btnVideoBaslat);
+        btnVideoDurdur = findViewById(R.id.btnDurdur);
+        btnVideoDevam = findViewById(R.id.btnVideoDevam);
+        btnStop = findViewById(R.id.btnStop);
+        btnResim1 = findViewById(R.id.btnResim1);
+        btnResim2 = findViewById(R.id.btnResim2);
     }
 }
