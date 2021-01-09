f0f586dda31f64f500a5275d606db31da8f6af76
oguzhandroid <oguzhaneem@gmail.com>
2021-01-09 23:50:56 +0300

    oncreate anında o anki tarih ve saat bilgisi alınarak ilgili text view lara yazdırıldı.

--- app/src/main/java/com/ogocer/widgets/MainActivity5.java
+++ app/src/main/java/com/ogocer/widgets/MainActivity5.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class MainActivity5 extends AppCompatActivity {
     private TextView tvSaat,tvTarih;
     private EditText etSaat,etTarih;
@@ -19,5 +21,18 @@ public class MainActivity5 extends AppCompatActivity {
         tvTarih = findViewById(R.id.tvTarih);
         etSaat = findViewById(R.id.etSaat);
         etTarih = findViewById(R.id.etTarih);
+
+        Calendar calendar = Calendar.getInstance();
+        int saat = calendar.get(Calendar.HOUR_OF_DAY);
+        int dakika = calendar.get(Calendar.MINUTE);
+        int yil = calendar.get(Calendar.YEAR);
+        int ay = calendar.get(Calendar.MONTH);
+        int gun = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvSaat.setText(saat + " : "+dakika);
+        tvTarih.setText(gun+"/"+(ay+1)+"/"+yil);
+
+
+
     }
 }
